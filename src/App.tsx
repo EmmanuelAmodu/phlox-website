e5cf8d627687a8ce129e429a5ba30f54a7c153bf
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2024-02-13 10:46:39 -0500

    fix text

--- src/App.tsx
+++ src/App.tsx
@@ -29,10 +29,10 @@ function App() {
         <div className={classes('md:flex space-y-8 md:space-y-0 w-full', containerClassName)}>
           <div className='flex flex-col justify-center'>
             <h1 className='text-black font-extrabold font-sans text-[54px] leading-[66.8px] md:text-[64px] md:leading-[76.8px]'>
-              Experience <br className="hidden md:block"/>seamless<br/>
-              <span className="block bg-gradient-to-r from-tertiary via-secondary to-primary text-transparent bg-clip-text">Global Transaction</span>
+              Treasury  <br className="hidden md:block"/>management,<br/>
+              <span className="block bg-gradient-to-r from-tertiary via-secondary to-primary text-transparent bg-clip-text">liquidity infrastructure</span>
             </h1>
-            <p className="font-manrope text-[14px] leading-[22.78px] md:text-[24px] md:leading-[32.78px] text-sub-text mb-[45px]">An international financial connectivity.</p>
+            <p className="font-manrope text-[14px] leading-[22.78px] md:text-[24px] md:leading-[32.78px] text-sub-text mb-[45px]">and international financial connectivity.</p>
            <div> <Input placeholder="Enter your email" rightItem={<JoinWaitListButton className="max-sm:[&_span]:hidden "/>} /></div>
            <div className='mt-[24px]'>
             <div className="rounded-[32px] p-[12px] bg-white/50 inline-block">
